9da9ad22fa02d6709baaa6d1a3c168eebdd6f8ac
acheltonzuzi <acheltonzuzi@gmail.com>
2024-07-09 19:21:32 -0700

    a

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import streamlit as st
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
 
+load_dotenv()
 st.header('Buka assistant')
 prompt=st.text_input(label="Escreva aqui")
+llm = ChatAnthropic(
+    model="claude-3-haiku-20240307",
+    temperature=0,
+    #max_tokens=1024,
+    #timeout=None,
+    #max_retries=2,
+    # other params...
+)
+if prompt:
+    msg=llm.invoke(prompt)
+    st.write(msg.content)
