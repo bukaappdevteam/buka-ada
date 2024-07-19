83d12edfc26d514f98ad472eca7b57aa3ee87c99
acheltonzuzi <acheltonzuzi@gmail.com>
2024-07-19 04:22:16 -0700

    a

--- playground.py
+++ playground.py
@@ -1,57 +1,48 @@
 import streamlit as st
 from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
-from langchain_community.document_loaders import WebBaseLoader,TextLoader
+from langchain_community.document_loaders import WebBaseLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.runnables import RunnablePassthrough
 from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-#llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
-llm = ChatAnthropic(
+# llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
+llm = ChatOpenAI(model="gpt-4o-mini-2024-07-18", temperature=0)
+""" llm = ChatAnthropic(
     model="claude-3-haiku-20240307",
     temperature=0,
-    #max_tokens=1024,
-    #timeout=None,
-    #max_retries=2,
-    # other params...
-)
+) """
 ### Construct retriever ###
 
-loader = TextLoader('./cursos.txt',encoding='UTF-8')
+loader = TextLoader("./cursos.txt", encoding="UTF-8")
 
 docs = loader.load()
 
-from langchain_text_splitters import RecursiveCharacterTextSplitter 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1000,
-    chunk_overlap = 200,
-    add_start_index = True 
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
 )
 all_splits = text_splitter.split_documents(docs)
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-vectorstore = Chroma.from_documents(
-    documents = all_splits,
-    embedding = embeddings
-)
-retriever = vectorstore.as_retriever(
-    search_type='similarity',
-    search_kwargs={"k":6}
-)
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=embeddings)
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
 
 contextualize_q_system_prompt = """Given a chat history and the latest user question \
 which might reference context in the chat history, formulate a standalone question \
 which can be understood without the chat history. Do NOT answer the question, \
 just reformulate it if needed and otherwise return it as is."""
 
-contextualize_q_prompt= ChatPromptTemplate.from_messages(
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", contextualize_q_system_prompt),
         MessagesPlaceholder(variable_name="chat_history"),
@@ -62,7 +53,7 @@ contextualize_q_chain = contextualize_q_prompt | llm | StrOutputParser()
 
 contextualize_q_chain.invoke(
     {
-        "chat_history":[
+        "chat_history": [
             HumanMessage(content="What does LLM stand for?"),
             AIMessage(content="Large language model"),
         ],
@@ -123,24 +114,28 @@ qa_prompt = ChatPromptTemplate.from_messages(
         ("human", "{question}"),
     ]
 )
+
+
 def format_docs(docs):
     return "".join(doc.page_content for doc in docs)
+
 
 def contextualized_question(input: dict):
     if input.get("chat_history"):
         return contextualize_q_chain
     else:
         return input["question"]
-    
+
+
 rag_chain = (
     RunnablePassthrough.assign(
-        context = contextualized_question | retriever | format_docs
+        context=contextualized_question | retriever | format_docs
     )
-    | qa_prompt 
+    | qa_prompt
     | llm
 )
 
-#chat_history = []
+# chat_history = []
 
 
 """ while True:
@@ -162,23 +157,22 @@ rag_chain = (
     print("==================================")
     print(ai_msg.content) """
 
-st.set_page_config(page_title='Chat website')
-st.header('chat with website')
+st.set_page_config(page_title="Chat website")
+st.header("chat with website")
 
 
-if "chat_history"  not in st.session_state:
-    st.session_state.chat_history=[
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
         AIMessage(content="Ola, eu sou Alexandria, como posso te ajudar?")
     ]
 
-user_query=st.chat_input('o que desejas saber?')
-if user_query is not None and user_query!="":
-        #response=get_response(user_query)
-    response=rag_chain.invoke({
-        "question":user_query,
-        "chat_history":st.session_state.chat_history
-    })
+user_query = st.chat_input("o que desejas saber?")
+if user_query is not None and user_query != "":
+    # response=get_response(user_query)
+    response = rag_chain.invoke(
+        {"question": user_query, "chat_history": st.session_state.chat_history}
+    )
 
-        #st.write(response['answer'])
+    # st.write(response['answer'])
     st.session_state.chat_history.append(HumanMessage(content=user_query))
-    st.session_state.chat_history.append(AIMessage(content=response['answer']))
+    st.session_state.chat_history.append(AIMessage(content=response["answer"]))
