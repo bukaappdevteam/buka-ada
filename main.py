ed99775a6b2bf0f0f85666232a851857c07c9a1d
CJjumper <carlos.jaime.jumpers@gmail.com>
2024-08-21 23:21:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,191 +49,163 @@ vectorstore = FAISS.from_documents(documents=all_splits, embedding=embeddings)
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
 
 # Define response examples as a dictionary
-response_examples = {
-  "input": {
-    "channel": "Messenger",
-    "prompt": "olá"
+response_examples = [
+  {
+    "input": {
+      "channel": "Messenger",
+      "prompt": "Quais são todos os cursos disponíveis?"
+    },
+    "output": {
+      "channel": "Messenger",
+      "messages": [
+        {
+          "type": "text",
+          "content": "Excelente pergunta! Estou entusiasmada em apresentar-lhe nossa gama diversificada de cursos transformadores. Cada um deles foi criado não apenas para ensinar, mas para inspirar e capacitar. Aqui está uma visão geral dos nossos cursos:"
+        },
+        {
+          "type": "carousel",
+          "content": [
+            {
+              "title": "Curso de Recursos Humanos com Aplicação às Novas Tecnologias",
+              "description": "Lidere a revolução no RH, moldando o futuro da gestão de pessoas.",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/file-up-load.appspot.com/o/course-files%2Frecursos-humanas-tecnologias.jpeg?alt=media&token=d12998b8-de54-490a-b28f-ea29c060e185",
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Saiba Mais",
+                  "payload": "Me fale mais sobre o Curso de Recursos Humanos com Aplicação às Novas Tecnologias"
+                }
+              ]
+            },
+            {
+              "title": "Administração Windows Server 2022",
+              "description": "Domine a arte de gerenciar servidores e torne-se indispensável no mundo da TI.",
+              "image_url": "",
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Saiba Mais",
+                  "payload": "Me fale mais sobre o curso de Administração Windows Server 2022"
+                }
+              ]
+            },
+            {
+              "title": "Higiene e Segurança no Trabalho",
+              "description": "Torne-se um guardião da segurança, protegendo vidas e transformando ambientes de trabalho.",
+              "image_url": "",
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Saiba Mais",
+                  "payload": "Me fale mais sobre o curso de Higiene e Segurança no Trabalho"
+                }
+              ]
+            },
+            {
+              "title": "Curso de Power BI (Business Intelligence)",
+              "description": "Desbloqueie o poder dos dados e torne-se um visionário nos negócios.",
+              "image_url": "",
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Saiba Mais",
+                  "payload": "Me fale mais sobre o Curso de Power BI (Business Intelligence)"
+                }
+              ]
+            },
+            {
+              "title": "Curso Base de Dados Relacional com MySQL",
+              "description": "Torne-se um mestre em dados, construindo a espinha dorsal da era digital.",
+              "image_url": "",
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Saiba Mais",
+                  "payload": "Me fale mais sobre o Curso Base de Dados Relacional com MySQL"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "text",
+          "content": "Cada um destes cursos é uma porta para um futuro brilhante. Qual deles faz seu coração bater mais forte? Algum chamou-lhe a atenção? Ou há alguma área específica da sua vida ou carreira que você sonha em transformar?"
+        }
+      ],
+      "internal_notes": "Estágio do Funil de Vendas: Interesse. Insights Importantes do Cliente: Interessado em conhecer todas as opções disponíveis. Próximos Passos: Identificar aspirações profundas do cliente para alinhar com os benefícios transformadores dos cursos."
+    }
   },
-  "output": {
-    "channel": "Messenger",
-    "messages": [
-      {
-        "type": "text",
-        "content": "Olá! Bem-vindo à Buka, onde não apenas ensinamos, mas mudamos vidas por meio da educação. Sou a Ada, assistente IA virtual da Buka, e sua guia pessoal nesta jornada emocionante de descoberta e crescimento. Estou aqui para ajudá-lo(a) a encontrar o curso perfeito que não só impulsionará sua carreira e/ou futuro, mas também realizará seus objetivos mais profundos."
-      },
-      {
-        "type": "text",
-        "content": "Temos uma variedade incrível de cursos disponíveis. E cada curso foi cuidadosamente projetado para oferecer não apenas conhecimentos, mas verdadeiras ferramentas de mudança de vida."
-      },
-      {
-        "type": "text",
-        "content": "Estou curiosa: o que o(a) traz à Buka hoje? Está em busca de uma transformação profissional específica ou está aberto a explorar novas possibilidades?"
-      }
-    ],
-    "internal_notes": "Estágio do Funil de Vendas: Consciencialização. Insights Importantes do Cliente: Ainda não há informações específicas. Próximos Passos: Identificar motivações profundas do cliente e alinhar com nossas ofertas."
+  {
+    "input": {
+      "channel": "Messenger",
+      "prompt": "Fale-me mais sobre o curso de Power BI"
+    },
+    "output": {
+      "channel": "Messenger",
+      "messages": [
+        {
+          "type": "text",
+          "content": "Estou realmente entusiasmada com o seu interesse no Curso de Power BI (Business Intelligence)! Você está prestes a embarcar numa jornada que pode revolucionar não apenas sua carreira, mas toda a forma como você vê e interage com o mundo dos dados. Permita-me compartilhar mais sobre esta experiência transformadora:"
+        },
+        {
+          "type": "text",
+          "content": "**Curso de Power BI (Business Intelligence)**"
+        },
+        {
+          "type": "text",
+          "content": "- **Descrição:** Explore o universo dos dados com o Power BI. Transforme informações em estratégias inteligentes e leve sua carreira ou empresa ao sucesso."
+        },
+        {
+          "type": "text",
+          "content": "- **Formato/Localização:** Presencial, na Digital.AO, Bairro CTT, Rangel, Luanda, Angola"
+        },
+        {
+          "type": "text",
+          "content": "- **Preço:** 60.000 Kz - um investimento que pode multiplicar seu valor profissional exponencialmente"
+        },
+        {
+          "type": "text",
+          "content": "- **Duração:** 2 Semanas intensivas (03 a 10 de Agosto 2024)"
+        },
+        {
+          "type": "text",
+          "content": "- **Horário:** Sábados, das 09:00 às 13:00"
+        },
+        {
+          "type": "list",
+          "content": [
+            "Fundamentos de Power BI e Configuração Inicial - Construindo sua base de poder",
+            "Visualizações e Publicação - Transformando números em narrativas visuais impactantes",
+            "Aprofundamento na Modelagem de Dados - Dominando a arte de estruturar informações",
+            "Design Avançado de Relatórios e Dashboards - Criando insights que impactam"
+          ]
+        },
+        {
+          "type": "text",
+          "content": "Estamos falando de mais do que apenas números e gráficos. O Power BI é uma ferramenta de transformação que pode reconfigurar o futuro de um negócio ou carreira. Pronto para dominar a arte dos dados?"
+        },
+        {
+          "type": "buttons",
+          "content": [
+            {
+              "title": "Inscreva-se Agora",
+              "type": "postback",
+              "payload": "Inscrição no Curso de Power BI"
+            }
+          ]
+        },
+        {
+          "type": "text",
+          "content": "Caso precise de mais informações ou esteja pronto(a) para fazer a inscrição, estou aqui para ajudar em cada etapa. O futuro espera por você!"
+        }
+      ],
+      "internal_notes": "Estágio do Funil de Vendas: Consideração. Insights Importantes do Cliente: Interessado no Curso de Power BI. Próximos Passos: Incentivar a decisão de inscrição, destacando benefícios chave e oferecendo suporte completo."
+    }
   }
-},
-{
-  "input": {
-    "channel": "Messenger",
-    "prompt": "Quais são todos os cursos disponíveis?"
-  },
-  "output": {
-    "channel": "Messenger",
-    "messages": [
-      {
-        "type": "text",
-        "content": "Excelente pergunta! Estou entusiasmada em apresentar-lhe nossa gama diversificada de cursos transformadores. Cada um deles foi criado não apenas para ensinar, mas para inspirar e capacitar. Aqui está uma visão geral dos nossos cursos:"
-      },
-      {
-        "type": "carousel",
-        "content": [
-          {
-            "title": "Curso de Recursos Humanos com Aplicação às Novas Tecnologias",
-            "description": "Lidere a revolução no RH, moldando o futuro da gestão de pessoas.",
-            "image_url": "https://firebasestorage.googleapis.com/v0/b/file-up-load.appspot.com/o/course-files%2Frecursos-humanas-tecnologias.jpeg?alt=media&token=d12998b8-de54-490a-b28f-ea29c060e185",
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Saiba Mais",
-                "payload": "Me fale mais sobre o Curso de Recursos Humanos com Aplicação às Novas Tecnologias"
-              }
-            ]
-          },
-          {
-            "title": "Administração Windows Server 2022",
-            "description": "Domine a arte de gerenciar servidores e torne-se indispensável no mundo da TI.",
-            "image_url": "",
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Saiba Mais",
-                "payload": "Me fale mais sobre o curso de Administração Windows Server 2022"
-              }
-            ]
-          },
-          {
-            "title": "Higiene e Segurança no Trabalho",
-            "description": "Torne-se um guardião da segurança, protegendo vidas e transformando ambientes de trabalho.",
-            "image_url": "",
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Saiba Mais",
-                "payload": "Me fale mais sobre o curso de Higiene e Segurança no Trabalho"
-              }
-            ]
-          },
-          {
-            "title": "Curso de Power BI (Business Intelligence)",
-            "description": "Desbloqueie o poder dos dados e torne-se um visionário nos negócios.",
-            "image_url": "",
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Saiba Mais",
-                "payload": "Me fale mais sobre o Curso de Power BI (Business Intelligence)"
-              }
-            ]
-          },
-          {
-            "title": "Curso Base de Dados Relacional com MySQL",
-            "description": "Torne-se um mestre em dados, construindo a espinha dorsal da era digital.",
-            "image_url": "",
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Saiba Mais",
-                "payload": "Me fale mais sobre o Curso Base de Dados Relacional com MySQL"
-              }
-            ]
-          }
-        ]
-      },
-      {
-        "type": "text",
-        "content": "Cada um destes cursos é uma porta para um futuro brilhante. Qual deles faz seu coração bater mais forte? Algum chamou-lhe a atenção? Ou há alguma área específica da sua vida ou carreira que você sonha em transformar?"
-      }
-    ],
-    "internal_notes": "Estágio do Funil de Vendas: Interesse. Insights Importantes do Cliente: Interessado em conhecer todas as opções disponíveis. Próximos Passos: Identificar aspirações profundas do cliente para alinhar com os benefícios transformadores dos cursos."
-  }
-},
-{
-  "input": {
-    "channel": "Messenger",
-    "prompt": "Fale-me mais sobre o curso de Power BI"
-  },
-  "output": {
-    "channel": "Messenger",
-    "messages": [
-      {
-        "type": "text",
-        "content": "Estou realmente entusiasmada com o seu interesse no Curso de Power BI (Business Intelligence)! Você está prestes a embarcar numa jornada que pode revolucionar não apenas sua carreira, mas toda a forma como você vê e interage com o mundo dos dados. Permita-me compartilhar mais sobre esta experiência transformadora:"
-      },
-      {
-        "type": "text",
-        "content": "**Curso de Power BI (Business Intelligence)**"
-      },
-      {
-        "type": "text",
-        "content": "- **Descrição:** Explore o universo dos dados com o Power BI. Transforme informações em estratégias inteligentes e leve sua carreira ou empresa ao sucesso."
-      },
-      {
-        "type": "text",
-        "content": "- **Formato/Localização:** Presencial, na Digital.AO, Bairro CTT, Rangel, Luanda, Angola"
-      },
-      {
-        "type": "text",
-        "content": "- **Preço:** 60.000 Kz - um investimento que pode multiplicar seu valor profissional exponencialmente"
-      },
-      {
-        "type": "text",
-        "content": "- **Duração:** 2 Semanas intensivas (03 a 10 de Agosto 2024)"
-      },
-      {
-        "type": "text",
-        "content": "- **Horário:** Sábados, das 09:00 às 13:00"
-      },
-      {
-        "type": "list",
-        "content": [
-          "Fundamentos de Power BI e Configuração Inicial - Construindo sua base de poder",
-          "Visualizações e Publicação - Transformando números em narrativas visuais impactantes",
-          "Aprofundamento na Modelagem de Dados - Dominando a arte de estruturar informações",
-          "Design Avançado de Relatórios e Dashboards - Criando insights que impactam"
-        ]
-      },
-      {
-        "type": "text",
-        "content": "Estamos falando de mais do que apenas números e gráficos. O Power BI é uma ferramenta de transformação que pode reconfigurar o futuro de um negócio ou carreira. Pronto para dominar a arte dos dados?"
-      },
-      {
-        "type": "buttons",
-        "content": [
-          {
-            "title": "Inscreva-se Agora",
-            "type": "postback",
-            "payload": "Inscrição no Curso de Power BI"
-          }
-        ]
-      },
-      {
-        "type": "text",
-        "content": "Caso precise de mais informações ou esteja pronto(a) para fazer a inscrição, estou aqui para ajudar em cada etapa. O futuro espera por você!"
-      }
-    ],
-    "internal_notes": "Estágio do Funil de Vendas: Consideração. Insights Importantes do Cliente: Interessado no Curso de Power BI. Próximos Passos: Incentivar a decisão de inscrição, destacando benefícios chave e oferecendo suporte completo."
-  }
-}
+]
 
 # Convert response examples to JSON string
 response_examples_json = json.dumps(response_examples, ensure_ascii=False, indent=4)
-
-# Set the page configuration
-st.set_page_config(
-    page_title="Buka Chatbot",
-    layout="wide"
-)
 
 # Define system prompt with dynamic examples
 qa_system_prompt = f"""You are Ada, an exceptional AI sales representative for Buka, an edtech startup dedicated to transforming lives through education. Your persona blends the persuasive skills of Jordan Belfort, the inspirational approach of Simon Sinek, and the visionary spirit of Steve Jobs. Your task is to engage with potential customers and effectively sell courses.
@@ -267,7 +239,7 @@ Your responses should be structured as JSON containing:
 
 #### Example Response:
 
-
+{response_examples_json}
 
 Follow these steps to interact with the customer:
 
@@ -340,7 +312,7 @@ if user_query is not None and user_query != "":
       "input": user_query,
       "chat_history": st.session_state.chat_history,
       "context": context,
-      #"response_examples_json": response_examples_json
+      "response_examples_json": response_examples_json
   }
 
   # Use StreamlitCallbackHandler to display intermediate steps
