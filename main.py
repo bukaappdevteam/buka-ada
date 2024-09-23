a3fa9ab5a90b47083f30ad7515eabf44186050cb
CJjumper <carlos.jaime.jumpers@gmail.com>
2024-09-23 15:26:16 +0100

    Update main.py to send from python server instead of n8n

--- main.py
+++ main.py
@@ -46,6 +46,17 @@ class UserQuery(BaseModel):
     prompt: str
 
 
+#Chatwoot
+
+class RequestBodyChatwoot(BaseModel):
+    url_chatwoot_: str
+    token_chatwoot: str
+    account_chatwoot: str
+    conversation_id: str
+    phone: str
+    prompt: str
+
+
 #BotConversa
 
 headersBotConversa = {
@@ -59,6 +70,7 @@ class RequestBodyBotConversa(BaseModel):
     subscriber_id: str = Field(default=None,
                                description="Optional Subscriber ID")
     prompt: str
+
 
 
 def get_phone_url(phone: str) -> str:
@@ -795,6 +807,69 @@ async def send_message(user_query: RequestBodyBotConversa):
                 send_response.raise_for_status()
 
         return {"success": True, "subscriber_id": subscriber_id}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/chat/bot-whatsapp")
+async def send_message(user_query: RequestBodyChatwoot):
+    # Prepare the input for the agent
+    context_docs = await asyncio.to_thread(retriever.get_relevant_documents,
+                                           user_query.prompt)
+    context = "\n".join([doc.page_content for doc in context_docs])
+
+    chat_history_list = chat_history['user_id']  # Alterado de str para lista
+
+    try:
+        response = await asyncio.wait_for(asyncio.to_thread(
+            chain.invoke, {
+                "input": user_query.prompt,
+                "chat_history": chat_history_list,
+                "CONTEXT": context,
+                "RESPONSE_EXAMPLES_JSON": response_examples_botconversa_json,
+                "CHANNEL": "whatsapp",
+                "COURSES": cached_get_courses(),
+                "agent_scratchpad": []
+            }),
+                                          timeout=15)
+    except asyncio.TimeoutError:
+        raise HTTPException(status_code=408,
+                            detail="Tempo de resposta excedido")
+
+    try:
+        # Acessar o conteúdo da resposta corretamente
+        response_content = response.content if isinstance(
+            response, AIMessage) else response["output"]
+        response_json = json.loads(response_content)
+
+        # Adicionar a resposta ao histórico de mensagens
+        chat_history["user_id"].append(HumanMessage(content=user_query.prompt))
+        chat_history["user_id"].append(AIMessage(content=response_content))
+        messages = response_json.get("messages", [])
+
+
+        # Send the messages to ManyChat API
+        headers = {
+            
+            "Content-Type": "application/json",
+            "api_access_token": user_query.api_access_token
+        }
+
+        async with httpx.AsyncClient() as client:
+            for message in messages:
+
+                message_data = {
+                    "content": message
+                }
+
+                send_response = await client.post(
+                    user_query.api_url,
+                    json=message_data,
+                    headers=headers,
+                )
+                send_response.raise_for_status()
+
+        return {"success": True}
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
